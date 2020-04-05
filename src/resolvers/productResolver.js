a4924f30e459726632f27da03637ded3d3b21d98
ปัจวัฒน์ ศรีบุญเรือง <patjawat@Macintosh.local>
2020-04-05 21:01:21 +0700

    udpate

--- src/resolvers/productResolver.js
+++ src/resolvers/productResolver.js
@@ -1,12 +1,28 @@
-export default  {
-    Query:{
-        allProduct:(_,args,{models}) => {
+export default {
+    Query: {
+        allProduct: (_, args, { models }) => {
             return models.Product.findAll()
-        }
+        },
+        getProduct: (root, { id }, { models }) => {
+            return models.Product.findByPk(id)
+        },
     },
-    Mutation:{
-        createProduct:(_,args, {models}) =>{
+    Mutation: {
+        createProduct: (_, args, { models }) => {
             return models.Product.create(args)
+        },
+        updateProduct: (_, args,  { id },{ models }) => {
+            return models.Product.findByPk(id);
+            // models.Product.update({
+            //         name: name,
+            //         price: price
+            //     },
+            //     {
+            //         where: {
+            //             id: id
+            //         }
+            //     });
+            },
         }
-    }
+    
 }
