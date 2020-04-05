cc823df82b037851ac0acc8958faa5d01d969b0f
patjawat <patjawat@gmail.com>
2020-04-05 22:12:24 +0700

    update

--- src/resolvers/productResolver.js
+++ src/resolvers/productResolver.js
@@ -11,18 +11,24 @@ export default {
         createProduct: (_, args, { models }) => {
             return models.Product.create(args)
         },
-        updateProduct: (_, args,  { id },{ models }) => {
-            return models.Product.findByPk(id);
-            // models.Product.update({
-            //         name: name,
-            //         price: price
-            //     },
-            //     {
-            //         where: {
-            //             id: id
-            //         }
-            //     });
+        updateProduct: (root,  { id,name,price },{ models }) => {
+            return models.Product.update({
+                    name: name,
+                    price: price
+                },
+                {
+                    where: {
+                        id: id
+                    }
+                });
             },
-        }
+            deleteProduct: (root, {id}, { models }, info) =>{
+                models.Product.destroy({
+                    where: {
+                        id: id
+                    }
+                });
+            },
+        },
     
 }
